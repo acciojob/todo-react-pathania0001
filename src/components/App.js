15ff60722b4f29f51b75da28a9844c223dd15136
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-08-28 18:10:09 +0530

    submition

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Todo from "./Todo";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]); 
 
-export default App
+  
+  const addTodo = () => {
+    if (task.trim() === "") return;
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+ 
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="app">
+      <h1>React To-Do List</h1>
+
+      <div className="input-section">
+        <input
+          type="text"
+          value={task}
+          placeholder="Enter a task"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+
+      <Todo todos={todos} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
